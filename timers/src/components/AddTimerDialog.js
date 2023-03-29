88332bb14c83d63a3ca7d0c04e84023ebb645802
Javid Ahmed <javidahmed@icloud.com>
2023-03-30 00:48:17 +0100

    Begin to add dialog to add timers

---
+++ timers/src/components/AddTimerDialog.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Box from "@mui/material/Box";
+import AddIcon from "@mui/icons-material/Add";
+import StyledIconButton from "./StyledIconButton";
+
+export default function AddTimerDialog(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box>
+      <StyledIconButton
+        variant="contained"
+        icon={<AddIcon fontSize="inherit" />}
+        onClick={handleClickOpen}
+        size={"large"}
+        iconColor={props.colour_primary}
+        borderColour={props.colour_tertiary}
+      />
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add Timer</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here.
+            We will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+}
