96a88368f3b1e1e7e234c4ff246f2a0dd99463c4
Javid Ahmed <javidahmed@icloud.com>
2023-04-02 02:52:59 +0100

    Wrap timer in box

--- timers/src/components/TimerListItem.js
+++ timers/src/components/TimerListItem.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Card } from "@mui/material";
 import { Stack } from "@mui/system";
+import Box from "@mui/material/Box";
 import TimerDisplay from "./TimerDisplay";
 import { parseTimeText } from "../utils/TimerUtils";
 import {
@@ -30,29 +31,31 @@ export default function TimerListItem(props) {
   }, [running, remainingTime, dispatch, props.id]);
 
   return (
-    <Card
-      variant="outlined"
-      style={{
-        backgroundColor: props.colour_secondary,
-        border: "1px solid",
-        borderColor: props.colour_text,
-        margin: 2,
-      }}
-    >
-      <Stack justifyContent="center" alignItems="center">
-        <TimerDisplay
-          key={props.id}
-          id={props.id}
-          label={props.label}
-          hoursText={parseTimeText(remainingTime).hoursText}
-          minutesText={parseTimeText(remainingTime).minutesText}
-          secondsText={parseTimeText(remainingTime).secondsText}
-          colour_primary={props.colour_primary}
-          colour_tertiary={props.colour_tertiary}
-          colour_text={props.colour_text}
-          backgroundColor={props.backgroundColor}
-        />
-      </Stack>
-    </Card>
+    <Box display="inline-block">
+      <Card
+        variant="outlined"
+        style={{
+          backgroundColor: props.colour_secondary,
+          border: "1px solid",
+          borderColor: props.colour_text,
+          margin: 2,
+        }}
+      >
+        <Stack justifyContent="center" alignItems="center">
+          <TimerDisplay
+            key={props.id}
+            id={props.id}
+            label={props.label}
+            hoursText={parseTimeText(remainingTime).hoursText}
+            minutesText={parseTimeText(remainingTime).minutesText}
+            secondsText={parseTimeText(remainingTime).secondsText}
+            colour_primary={props.colour_primary}
+            colour_tertiary={props.colour_tertiary}
+            colour_text={props.colour_text}
+            backgroundColor={props.backgroundColor}
+          />
+        </Stack>
+      </Card>
+    </Box>
   );
 }
